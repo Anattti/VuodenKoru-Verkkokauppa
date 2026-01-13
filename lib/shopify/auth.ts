9f185cc5ef22c9dae2881b0a3d2c812a86ade646
Anattti <antsa_93_@hotmail.com>
2026-01-13 17:27:37 +0200

    final: cleanup debug logs and finalize logout flow

--- lib/shopify/auth.ts
+++ lib/shopify/auth.ts
@@ -258,7 +258,6 @@ export async function logout() {
         const logoutUrl = `https://shopify.com/authentication/${SHOP_ID}/logout?id_token_hint=${session.idToken}&post_logout_redirect_uri=${encodeURIComponent(returnUrl)}`;
         redirect(logoutUrl);
     } else {
-        // Jos idTokenia ei jostain syystä ole, ohjataan login-sivulle dynaamisesti
         redirect('/account/login?status=logged_out');
     }
 }
